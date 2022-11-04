a45dcd1e03907c9b273888b2f6d81e798c734e5e
Uk0106 <Umang.kala@gmail.com>
2022-11-04 21:01:03 +0530

    First Commit

---
+++ src/main/java/Finance/ConsumerFinanceSystem/ConsumerFinanceSystemApplication.java
@@ -0,0 +1,13 @@
+package Finance.ConsumerFinanceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerFinanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerFinanceSystemApplication.class, args);
+	}
+
+}
